178d7b67faf295a141c61cc5bd648a3cdb77adb2
jenny78930 <jezh0928@gmail.com>
2025-04-24 14:28:02 +0200

    Update CashPayment.java
    comments

--- src/main/java/se/kth/iv1350/cashiersystem/model/CashPayment.java
+++ src/main/java/se/kth/iv1350/cashiersystem/model/CashPayment.java
@@ -1,10 +1,21 @@
 package se.kth.iv1350.cashiersystem.model;
 
+
+/**
+* Represents a payment made by cash y the customer.
+*/
 public class CashPayment {
     private float amountPaid;
     private float totalPrice;
     private float change;
 
+/**
+* Creates a new instance of CashPayment.
+* Calls the method to calculate the change based on the amount paid and the total price.
+*
+* @param amountPaid The amount paid by the customer.
+* @param totalPrice The total price of the sale.
+*/
     public CashPayment(float amountPaid, float totalPrice) {
         this.amountPaid = amountPaid;
         this.totalPrice = totalPrice;
@@ -12,18 +23,36 @@ public class CashPayment {
         calculateChange();
     }
 
+/**
+* Calculates the amount of change that the customer is receiving
+*/
     public void calculateChange() {
         change = amountPaid - totalPrice;
     }
 
+/**
+* Gets the change of the sale.
+*
+* @return The change of the sale.
+*/
     public float getChange() {
         return change;
     }
 
+/**
+* Gets the amount paid by the customer.
+*
+* @return The amount paid by the customer.
+*/
     public float getAmountPaid() {
         return amountPaid;
     }
-
+    
+/**
+* Gets the total price of the sale.
+*
+* @return The total price of the sale.
+*/
     public float getTotalPrice() {
         return totalPrice;
     }
