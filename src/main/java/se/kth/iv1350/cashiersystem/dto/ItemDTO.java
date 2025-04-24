9b71c1075715ff028fa41c16b83b77e54c48e32a
jenny78930 <jezh0928@gmail.com>
2025-04-24 13:51:36 +0200

    Update ItemDTO.java
    comments

--- src/main/java/se/kth/iv1350/cashiersystem/dto/ItemDTO.java
+++ src/main/java/se/kth/iv1350/cashiersystem/dto/ItemDTO.java
@@ -3,14 +3,13 @@ package se.kth.iv1350.cashiersystem.dto;
 import se.kth.iv1350.cashiersystem.model.Item;
 
 /**
- * DTO for the item class.
- * This class is used to transfer item data between the integration layer and the model layer
- *
+ * DTO for item details
+ * This class is used to transfer item data between the integration layer and the model layer.
  * It includes details about an item such as ID, name, price, description, and VAT percentage.
  */
 public record ItemDTO(String id, String name, float price, String description, int vatPercentage) {
 /**
-* Converts this ItemDTO to an Item object used in the model layer.
+* Converts this ItemDTO to an Item object
 *
 * @return A new Item object containing the same data as this DTO.
 */
