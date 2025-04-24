402828511b85d6635d24004e99e3848377b7e8c4
jenny78930 <jezh0928@gmail.com>
2025-04-24 14:10:19 +0200

    Update RegistryCreator.java
    comments

--- src/main/java/se/kth/iv1350/cashiersystem/integration/RegistryCreator.java
+++ src/main/java/se/kth/iv1350/cashiersystem/integration/RegistryCreator.java
@@ -1,16 +1,26 @@
 package se.kth.iv1350.cashiersystem.integration;
 
 /**
- *
+ * Creates and provides access to external registries
  */
 public class RegistryCreator {
     private final InventoryRegistryHandler inventoryRegistry = new InventoryRegistryHandler();
     private final AccountingRegistryHandler accountingRegistry = new AccountingRegistryHandler();
 
+    /**
+     * Provides access to the InventoryRegistryHandler.
+     * 
+     * @return The instance of InventoryRegistryHandler
+     */
     public InventoryRegistryHandler getInventoryRegistry() {
         return inventoryRegistry;
     }
 
+    /**
+     * Provides access to the AccountingRegistryHandler.
+     * 
+     * @return The instance of AccountingRegistryHandler
+     */
     public AccountingRegistryHandler getAccountingRegistry() {
         return accountingRegistry;
     }
