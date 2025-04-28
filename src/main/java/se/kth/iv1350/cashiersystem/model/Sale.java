46d310545eb0e1eae6f7022a107c9c63e94e698e
jenny78930 <jezh0928@gmail.com>
2025-04-28 18:29:23 +0200

    Update Sale.java

--- src/main/java/se/kth/iv1350/cashiersystem/model/Sale.java
+++ src/main/java/se/kth/iv1350/cashiersystem/model/Sale.java
@@ -6,6 +6,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+* Represents an ongoing sale process.
+*/
 public class Sale {
     private Collection<Item> itemsInCart = new ArrayList<>();
     private CashPayment cashPayment;
@@ -15,15 +18,33 @@ public class Sale {
     //Not in seminar 3.
     private float discount = 0;
 
+    /**
+    * Checks if the item is already in the current sale.
+    *
+    * @param itemId The identification of the item.
+    * @return <code>true</code> if the item is already in the sale, otherwise <code>false</code>.
+    */
     public boolean isItemInSale(String itemId) {
         Item item = findItemInCart(itemId);
         return item != null;
     }
 
+    /**
+    * Sets the cash payment for the sale.
+    *
+    * @param cashPayment The <code>CashPayment</code> object containing payment details.
+    */
     public void setCashPayment(CashPayment cashPayment) {
         this.cashPayment = cashPayment;
     }
 
+    /**
+    * Updates the quantity of an existing item in the sale.
+    * Updates the running total and the total VAT after updating the quantity.
+    *
+    * @param itemId   The identification of the item.
+    * @param quantity The quantity to add.
+    */
     public void updateQuantity(String itemId, int quantity) {
         Item item = findItemInCart(itemId);
         item.addQuantity(quantity);
@@ -40,6 +61,14 @@ public class Sale {
         return null;
     }
 
+
+    /**
+    * Adds a new item to the sale.
+    * Updates the running total and the total VAT after adding.
+    *
+    * @param item     The <code>Item</code> to add.
+    * @param quantity The <code>quantity</code> of the item to add which is default 1.
+    */
     public void addItem(Item item, int quantity) {
         item.setQuantity(quantity);
         itemsInCart.add(item);
@@ -47,10 +76,20 @@ public class Sale {
         calculateVatTotal();
     }
 
+    /**
+    * Gets the current running total of the sale.
+    *
+    * @return The running total of the sale.
+    */
     public float getRunningTotal() {
         return runningTotal;
     }
 
+    /**
+    * Gets the total VAT of the sale.
+    *
+    * @return The total VAT of the sale.
+    *
     public float getVatTotal() {
         return vatTotal;
     }
@@ -76,6 +115,11 @@ public class Sale {
         return price * ((float) vatPercentage / 100);
     }
 
+    /**
+    * Ends the sale.
+    *
+    * @return The total price of the sale.
+    */
     public float endSale() {
         return runningTotal;
     }
