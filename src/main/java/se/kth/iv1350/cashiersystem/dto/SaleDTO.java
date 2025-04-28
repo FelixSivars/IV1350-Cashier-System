fbe95e433f9ebdde06c8fb478a6ed7d7e89535bd
jenny78930 <jezh0928@gmail.com>
2025-04-28 14:28:04 +0200

    Update SaleDTO.java
    comments

--- src/main/java/se/kth/iv1350/cashiersystem/dto/SaleDTO.java
+++ src/main/java/se/kth/iv1350/cashiersystem/dto/SaleDTO.java
@@ -5,34 +5,73 @@ import se.kth.iv1350.cashiersystem.model.Item;
 import java.time.LocalDateTime;
 import java.util.Collection;
 
-// NOT IN USE
-// Map<String, Integer> itemQuantityMap, String = ItemID & Integer = Quantity.
+ /**
+ * DTO for sale details
+ * This class is used to transfer item data between the integration layer and the model layer.
+ * It includes details about a sale such as date and time of the sale, running total, vat total, amount paid, change given, discount applied and the items in the cart.
+ */
 public record SaleDTO(LocalDateTime dateTime, float runningTotal, float vatTotal,
                       float amountPaid, float change, float discount, Collection<Item> itemsInCart) {
+   
+     /**
+     * Gets the date and time when the sale has ended
+     * 
+     * @return date and time at the end of the sale.
+     */
     public LocalDateTime getDateTime() {
         return dateTime;
     }
-
+  
+     /**
+     * Gets the current running total of the sale
+     * 
+     * @return The total cost of all items in the sale
+     */
     public float getRunningTotal() {
         return runningTotal;
     }
-
+  
+     /**
+     * Gets the total VAT for the sale.
+     * 
+     * @return The total VAT amount for the sale.
+     */
     public float getVatTotal() {
         return vatTotal;
     }
-
+  
+     /**
+     * Gets the amount paid by the customer
+     * 
+     * @return The amount of money paid by the customer
+     */
     public float getAmountPaid() {
         return amountPaid;
     }
-
+  
+     /**
+     * Gets the amount of change that the customer is receiving
+     * 
+     * @return The change returned to the customer after payment.
+     */
     public float getChange() {
         return change;
     }
 
+     /**
+     * Gets the discount applied to the sale.
+     * 
+     * @return The total discount amount applied to the sale.
+     */
     public float getDiscount() {
         return discount;
     }
-
+  
+     /**
+     * Gets the collection of items included in the sale.
+     * 
+     * @return A collection of item objects in the cart
+     */
     public Collection<Item> getItemsInCart() {
         return itemsInCart;
     }
