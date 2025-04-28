42fec1f5173955b67815759a2f7188e1260324e2
jenny78930 <jezh0928@gmail.com>
2025-04-28 17:24:30 +0200

    Update AccountingRegistryHandler.java

--- src/main/java/se/kth/iv1350/cashiersystem/integration/AccountingRegistryHandler.java
+++ src/main/java/se/kth/iv1350/cashiersystem/integration/AccountingRegistryHandler.java
@@ -10,7 +10,8 @@ public class AccountingRegistryHandler {
      /**
      * Updates the accounting registry with the details of a completed sale that is used for accounting.
      *
-     * @param saleDTO The DTO containing all information about the completed sale that is needed to update the accounting registry.
+     * @param saleDTO The <code>SaleDTO</code> containing all information about the completed sale 
+     * that is needed to update the accounting registry.
      */
     public void updateAccountingRegistry(SaleDTO saleDTO) {
     //unknown to the assignment what it does exactly with the sale data to update the accounting registry
