67d07a4157c7d5593ca97635641dfaa97a63532a
jenny78930 <jezh0928@gmail.com>
2025-04-29 21:48:09 +0200

    Update RegistryCreator.java

--- src/main/java/se/kth/iv1350/cashiersystem/integration/RegistryCreator.java
+++ src/main/java/se/kth/iv1350/cashiersystem/integration/RegistryCreator.java
@@ -8,9 +8,9 @@ public class RegistryCreator {
     private final AccountingRegistryHandler accountingRegistryHandler = new AccountingRegistryHandler();
 
     /**
-     * Provides access to the <code>InventoryRegistryHandler</code>.
+     * Provides access to the <@link InventoryRegistryHandler>.
      *
-     * @return The instance of <code>InventoryRegistryHandler</code>
+     * @return The instance of <@link InventoryRegistryHandler>
      */
     public InventoryRegistryHandler getInventoryRegistryHandler() {
         return inventoryRegistryHandler;
