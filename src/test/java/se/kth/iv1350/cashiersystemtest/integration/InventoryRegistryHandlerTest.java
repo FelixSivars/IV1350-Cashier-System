e0710da4e786a8b269ee0c11831ea4d22f998f4a
Kai Nguyen <akakumura10@gmail.com>
2025-05-03 15:26:16 +0200

    - Fixed incorrect package name in InventoryRegistryHandlerTest.

--- src/test/java/se/kth/iv1350/cashiersystemtest/integration/InventoryRegistryHandlerTest.java
+++ src/test/java/se/kth/iv1350/cashiersystemtest/integration/InventoryRegistryHandlerTest.java
@@ -1,4 +1,4 @@
-package se.kth.iv1350.cashiersystemtest.controller;
+package se.kth.iv1350.cashiersystemtest.integration;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
