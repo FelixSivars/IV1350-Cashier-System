a4117e7a7ad6dc4db1c532fb70e59b32c5aa2e27
jenny78930 <jezh0928@gmail.com>
2025-05-03 16:40:00 +0200

    Update ItemInCartDTO.java
    - Updated comments

--- src/main/java/se/kth/iv1350/cashiersystem/dto/ItemInCartDTO.java
+++ src/main/java/se/kth/iv1350/cashiersystem/dto/ItemInCartDTO.java
@@ -3,14 +3,14 @@ package se.kth.iv1350.cashiersystem.dto;
 /**
  * DTO for items in cart
  * This class is used to transfer item data between the integration layer and the model layer.
- * It includes details about <@link ItemDTO> and the <code>quantity<code/> 
+ * It includes details about {@link ItemDTO} and the <code>quantity<code/> 
  */
 public record ItemInCartDTO(ItemDTO itemDTO, int quantity) {
     
     /**
-    * Gets <@link ItemDTO> from the cart
+    * Gets {@link ItemDTO} from the cart
     *
-    * @return <code>ItemDTO<code/> from the cart
+    * @return {@link ItemDTO} from the cart
     */
     public ItemDTO getItemDTO() {
         return itemDTO;
