f6fa5a2df709bd52344f79a160ca926832c39521
Kai Nguyen <akakumura10@gmail.com>
2025-05-03 16:11:19 +0200

    Created additional unit tests to Controller

--- src/test/java/se/kth/iv1350/cashiersystemtest/controller/ControllerTest.java
+++ src/test/java/se/kth/iv1350/cashiersystemtest/controller/ControllerTest.java
@@ -11,6 +11,7 @@ import se.kth.iv1350.cashiersystem.integration.InventoryRegistryHandler;
 import se.kth.iv1350.cashiersystem.integration.Printer;
 import se.kth.iv1350.cashiersystem.integration.RegistryCreator;
 
+import java.util.Collection;
 import java.util.Iterator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -44,6 +45,61 @@ public class ControllerTest {
 
         assertEquals("123", it.next().getItemDTO().id(),
                 "Item identifier should match the scanned item");
+    }
+
+    @Test
+    public void testScanRepeatedItem() {
+        // Scan the same item twice
+        controller.scanItem("123", 1);
+        controller.scanItem("123", 1);
+        
+        SaleDTO saleDTO = controller.getSaleDTO();
+        Collection<ItemInCartDTO> itemsInCart = saleDTO.getItemsInCartDTO();
+        
+        // Should still have only one unique item
+        assertEquals(1, itemsInCart.size(), "Should have exactly one unique item in cart");
+        
+        // Item quantity should be 2
+        ItemInCartDTO itemInCart = itemsInCart.iterator().next();
+        assertEquals(2, itemInCart.getQuantity(), "Item quantity should be 2");
+    }
+
+    @Test
+    public void testScanInvalidItem() {
+        // Try to scan an item with an ID that doesn't exist in inventory
+        controller.scanItem("nonexistent", 1);
+        
+        // Get sale information
+        SaleDTO saleDTO = controller.getSaleDTO();
+        Collection<ItemInCartDTO> itemsInCart = saleDTO.getItemsInCartDTO();
+        
+        // Verify that no item was added
+        assertEquals(0, itemsInCart.size(), "No item should be added when scanning invalid ID");
+    }
+
+    @Test
+    public void testMultipleItemsWithQuantities() {
+        controller.scanItem("123", 3);  // Add 3 of item 123
+        
+        // Set up a second test item in the inventory handler
+        ItemDTO secondItem = new ItemDTO("456", "Second Item", 20.0f, "Another item", 12);
+        InventoryRegistryHandler inventoryHandler = new InventoryRegistryHandler(secondItem);
+        controller.setInventoryRegistryHandler(inventoryHandler);
+        
+        controller.scanItem("456", 2);  // Add 2 of item 456
+        
+        // Get sale information
+        SaleDTO saleDTO = controller.getSaleDTO();
+        Collection<ItemInCartDTO> itemsInCart = saleDTO.getItemsInCartDTO();
+        
+        assertEquals(2, itemsInCart.size(), "Should have exactly two unique items in cart");
+        
+        // Check that the running total is calculated correctly
+        // First item: 3 * 14.90 = 44.7
+        // Second item: 2 * 20.0 = 40.0
+        // Total: 84.7
+        assertEquals(84.7f, saleDTO.getRunningTotal(), 0.01, 
+                "Running total should be sum of all items times their quantities");
     }
 
     @Test
