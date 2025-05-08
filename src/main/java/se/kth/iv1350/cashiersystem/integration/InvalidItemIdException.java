a6fddb25d5753bc65931e7f5c48021bc4cba865a
jenny78930 <jezh0928@gmail.com>
2025-05-08 19:42:08 +0200

    Update InvalidItemIdException.java
    - Added comments

--- src/main/java/se/kth/iv1350/cashiersystem/integration/InvalidItemIdException.java
+++ src/main/java/se/kth/iv1350/cashiersystem/integration/InvalidItemIdException.java
@@ -1,6 +1,17 @@
 package se.kth.iv1350.cashiersystem.integration;
 
+
+/**
+* A checked exception that is thrown when an invalid item ID is encountered during a sale.
+*/
 public class InvalidItemIdException extends Exception {
+
+    /**
+     * Creates a new instance of <code>InvalidItemIdException</code> with an error message
+     * indicating that the scanned item ID is invalid
+     *
+     * @param itemId The item ID that was not found in the inventory catalog.
+     */
     public InvalidItemIdException(String itemId) {
         super("The item with the id '" + itemId + "' is invalid.");
     }
