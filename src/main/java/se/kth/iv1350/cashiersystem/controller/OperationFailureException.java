fc79c33f4bc41ff4f63ac14ce2ca1be49751a1b0
jenny78930 <jezh0928@gmail.com>
2025-05-08 19:20:23 +0200

    Update OperationFailureException.java
    - Added comment

--- src/main/java/se/kth/iv1350/cashiersystem/controller/OperationFailureException.java
+++ src/main/java/se/kth/iv1350/cashiersystem/controller/OperationFailureException.java
@@ -1,6 +1,18 @@
 package se.kth.iv1350.cashiersystem.controller;
 
+/**
+* A checked exception that is thrown when a system operation fails,
+* provides data for error handling in the overall system.
+*/
+
 public class OperationFailureException extends Exception {
+
+    /**
+     * Creates a new instance of OperationFailureException
+     * with an error message and the cause of it being thrown.
+     *
+     * @param cause The exception that caused the system operation to fail.
+     */
     public OperationFailureException(Exception cause) {
         super("Failed to perform a system operation.", cause);
     }
