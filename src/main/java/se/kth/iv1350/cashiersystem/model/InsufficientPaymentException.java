850456ea77ba52c583e7952a6767d07c298847f5
jenny78930 <jezh0928@gmail.com>
2025-05-08 19:51:52 +0200

    Update InsufficientPaymentException.java
    - Added comment

--- src/main/java/se/kth/iv1350/cashiersystem/model/InsufficientPaymentException.java
+++ src/main/java/se/kth/iv1350/cashiersystem/model/InsufficientPaymentException.java
@@ -1,6 +1,17 @@
 package se.kth.iv1350.cashiersystem.model;
 
+/**
+ * A checked exception that is thrown when the amount paid by the customer is less than the total price of the sale.
+ */
 public class InsufficientPaymentException extends Exception {
+
+     /**
+     * Creates an instance of {@code InsufficientPaymentException} with a message that communicates the amount paid and 
+     * the total price required.
+     *
+     * @param amountPaid The amount paid by the customer.
+     * @param totalPrice The total price required for the purchase.
+     */
     public InsufficientPaymentException(float amountPaid, float totalPrice) {
         super("Insufficient payment of " + amountPaid + " SEK for the minimum required amount of " + totalPrice + " SEK.");
     }
